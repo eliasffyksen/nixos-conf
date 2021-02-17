a24c5efe144f4867420f06a2295a1f589919027f
Elias F. Fyksen <git@fyksen.net>
2021-02-17 09:07:43 +0000

    Add restart to uomvpn service

--- services.nix
+++ services.nix
@@ -9,5 +9,9 @@ in
     wantedBy = [ "multi-user.target" ];
     path = [ pkgs.openconnect pkgs.nettools pkgs.gawk ];
     script = "echo '${secrets.uomPwd}' | openconnect --protocol=gp vpnconnect-standard.manchester.ac.uk --user '${secrets.uomUser}' --passwd-on-stdin";
+    serviceConfig = {
+      Restart = "always";
+      RestartSec = 60;
+    };
   };
 }
